d5d0c033115e2b61e4cd7d4ffb8601cd22348ec8
non <a.netiphong@kkumail.com>
2019-04-01 21:28:35 +0700

    Update Classroom.php (Add student by id and excel)

--- teacher/add_student/add_student.php
+++ teacher/add_student/add_student.php
@@ -1,11 +1,11 @@
-<?php 
-	include("../../Level_page.php");
-	lavel_Page_L3();
-	include_once($level."connection/Connection.php");
-	include($level."include/main.php");
-	date_default_timezone_set('Asia/Bangkok');
-	session_start();
-	include_once($level."classroom/func_getMyClassroom.php");
+<?php
+include "../../Level_page.php";
+lavel_Page_L3();
+include_once $level . "connection/Connection.php";
+include $level . "include/main.php";
+date_default_timezone_set('Asia/Bangkok');
+session_start();
+include_once $level . "classroom/func_getMyClassroom.php";
 ?>
 <!doctype html>
 <html>
@@ -13,13 +13,13 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title><?=$domain_sub;?></title>
-	
+
 	<link href="../../bootstrap/css/bootstrap.css" rel="stylesheet">
 	<link href="../../css/style-main.css" rel="stylesheet">
 	<link href="../../css/navbar.css" rel="stylesheet">
 	<link href="../../css/product-list.css" rel="stylesheet">
 	<link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
-	
+
 	<style>
 		html,body{
 			width: 100%;
@@ -41,7 +41,7 @@
 			margin-bottom: 10px;
 			font-size: 15px;
 			border: 0px;
-		}		
+		}
 		.box_dash{
 			width: 250px;
 			height: 250px;
@@ -52,41 +52,44 @@
 		.box_dash:hover{
 			border: solid 2px;
 		}
-	</style>	
+	</style>
 </head>
 <body class="font bg_color-W2">
 
 	<?php
-		include($level."teacher/navbar_teacher.php"); 
-	?>
-	
+include $level . "teacher/navbar_teacher.php";
+?>
+
 	<div class="container-fluid">
 		<div class="row">
-		
-			<?php include($level."menu/menu.php");?> <!-- include menu.php -->
-			
+
+			<?php include $level . "menu/menu.php";?> <!-- include menu.php -->
+
 			<div class="col-md-9 bg_color-W3 pb-4" style="min-height: 100vh">
 				<div class="row">
 					<div class="col-3" ></div>
 					<div class="col-6 bg-dark text_color-main1 text-center py-2 mt-0" style="border-radius: 0px 0px 7px 7px">
-						<h5><i class="ion-ionic"></i> <?=getClassroom($_GET['class_id'])->subject_name ?></h5>
+						<h5><i class="ion-ionic"></i> <?=getClassroom($_GET['class_id'])->subject_name?></h5>
 					</div>
 					<div class="col-3"></div>
 				</div>
-				<div class="container">
-					<hr>
-					<div class="row pt-5 pb-3 text-center">
+				<div class="card text-center mt-5">
+					<div class="card-header">
+						เพิ่มนักศึกษาทีละคน
+					</div>
+						<div class="card-body">
+						<div class="row pt-5 pb-3 text-center">
 						<div class="col-md-3"></div>
 						<div class="col-md-6">
 							<form id="form_addTA" action="server_add_student.php" method="post" enctype="multipart/form-data">
 								<div class="col-md-12">
-									<input type="text" id="class_name" name="class_name" class="form-control" value="<?=getClassroom($_GET['class_id'])->subject_name ?>" readonly>
+									<input type="text" id="class_name" name="class_name" class="form-control" value="<?=getClassroom($_GET['class_id'])->subject_name?>" readonly>
 								</div>
 								<div class="col-md-12">
 									<input type="text" id="std_id" name="std_id" class="form-control" placeholder="รหัสนักศึกษา" required>
 								</div>
 								<div class="col-sm-12">
-									<p id="flag" style="color: red;"><?php if(isset($_GET['flag'])){echo $_GET['flag'];} ?></p>
+									<p id="flag" style="color: red;"><?php if (isset($_GET['flag'])) {echo $_GET['flag'];}?></p>
 								</div>
 								<div class="col-md-12">
 									<button name="addStd_submit" class="btn btn-main1" type="submit"><i class="ion-plus"></i> เพิ่มนักศึกษา </button>
@@ -96,20 +99,47 @@
 						</div>
 						<div class="col-md-3"></div>
 					</div>
-					
+					</div>
+
 				</div>
-			</div>
-		</div>
-	</div>
-	
+				<div class="container">
+					<hr>
+				</div>
+				<div class="card text-center">
+  					<div class="card-header">
+						เพิ่มนักศึกษาด้วยไฟล์ Excel
+					</div>
+					<div class="card-body">
+					<div class="card-body">
+					<div class="row pt-5 pb-3 text-center">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+  						<form id="form_addTA" action="server_add_student.php" method="post" enctype="multipart/form-data">
+								<div class="col-md-12">
+									<input type="file" id="std_id" name="fileToUpload" class="form-control" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" / required>
+								</div>
+								<div class="col-sm-12">
+									<p id="flag" style="color: red;"><?php if (isset($_GET['flag'])) {echo $_GET['flag'];}?></p>
+								</div>
+								<div class="col-md-12">
+									<button name="addStd_submit" class="btn btn-main1" type="submit"><i class="ion-plus"></i> อัพโหลด </button>
+								</div>
+								<input type="hidden" name="class_id" id="class_id" value="<?=$_GET['class_id']?>" >
+							</form>
+  						</div>
+						</div>
+						</div>
+						</div>
+						</div>
+
 	<script src="<?=$level;?>bootstrap/jquery-3.2.1.slim.min.js"></script>
-	<script src="<?=$level;?>bootstrap/popper.min.js"></script>	
+	<script src="<?=$level;?>bootstrap/popper.min.js"></script>
 	<script src="<?=$level;?>bootstrap/js/bootstrap.min.js"></script>
 	<script src="<?=$level;?>js/jquery.min.js"></script>
-	
-	<script>var level='<?php echo $level?>';</script>
+
+	<script>var level='<?php echo $level ?>';</script>
 	<script src="<?=$level?>js/count_cart.js"></script>
 	<script src="<?=$level?>js/delete_cart.js"></script>
-	
+
 </body>
 </html>
