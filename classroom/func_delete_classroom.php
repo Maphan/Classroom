e15a314fe08b63f9d263426545cb4535ca418769
non <a.netiphong@kkumail.com>
2019-04-02 01:25:50 +0700

    Update Delete class bug

--- classroom/func_delete_classroom.php
+++ classroom/func_delete_classroom.php
@@ -12,7 +12,7 @@ function delete_classroom($class_id){
     $stmt_delete_class_assistant=$GLOBALS['sql']->prepare("DELETE FROM teacher_assistant WHERE class_id=?;");
 	$stmt_delete_class_assistant->bindParam(1, $class_id);
     
-	if($stmt_delete_classroom->execute() && $stmt_delete_class_member->execute() && $stmt_delete_class_owner->execute()  && $stmt_delete_class_assistant){
+	if($stmt_delete_classroom->execute() && $stmt_delete_class_member->execute() && $stmt_delete_class_owner->execute()  && $stmt_delete_class_assistant->execute()){
 		return("true");
 	}else{
 		return("false");
