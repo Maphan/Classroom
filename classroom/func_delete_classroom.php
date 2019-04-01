5886979145c7dfe12588aa2385057b6ad86ce3d4
non <a.netiphong@kkumail.com>
2019-04-01 23:43:58 +0700

    Update home(show subject detail) & delete class

---
+++ classroom/func_delete_classroom.php
@@ -0,0 +1,21 @@
+<?php
+function delete_classroom($class_id){
+	$stmt_delete_classroom=$GLOBALS['sql']->prepare("DELETE FROM classroom WHERE class_id=?;");
+	$stmt_delete_classroom->bindParam(1, $class_id);
+    
+    $stmt_delete_class_member=$GLOBALS['sql']->prepare("DELETE FROM class_member WHERE class_id=?;");
+    $stmt_delete_class_member->bindParam(1, $class_id);
+    
+    $stmt_delete_class_owner=$GLOBALS['sql']->prepare("DELETE FROM owner_class WHERE class_id=?;");
+    $stmt_delete_class_owner->bindParam(1, $class_id);
+    
+    $stmt_delete_class_assistant=$GLOBALS['sql']->prepare("DELETE FROM teacher_assistant WHERE class_id=?;");
+	$stmt_delete_class_assistant->bindParam(1, $class_id);
+    
+	if($stmt_delete_classroom->execute() && $stmt_delete_class_member->execute() && $stmt_delete_class_owner->execute()  && $stmt_delete_class_assistant){
+		return("true");
+	}else{
+		return("false");
+	}
+}
+?>
--- teacher/home.php
+++ teacher/home.php
@@ -93,12 +93,21 @@
 									<span class="text-size-14"><?="[".$classroom_temp->year."/".$classroom_temp->term."]"?></span>
 								</div>
 								<div class="card-body pb-0">
-									<span class="card-text">Teacher  # คน</span><br>
-									<span class="card-text">Teacher assistant  # คน</span><br>
-									<span class="card-text">Student  # คน</span>
+									<?php $teacher_count=$sql->prepare("SELECT * FROM owner_class WHERE class_id=?"); ?>
+									<?php $teacher_count->bindParam(1,$classroom_temp->class_id);?>
+									<?php $teacher_count->execute(); ?>
+									<span class="card-text">Teacher <?php echo $teacher_count->rowCount();?> คน</span><br>
+									<?php $ta_count=$sql->prepare("SELECT * FROM teacher_assistant WHERE class_id=?"); ?>
+									<?php $ta_count->bindParam(1,$classroom_temp->class_id);?>
+									<?php $ta_count->execute(); ?>
+									<span class="card-text">Teacher assistant <?php echo $ta_count->rowCount();?> คน</span><br>
+									<?php $std_count=$sql->prepare("SELECT * FROM class_member WHERE class_id=?"); ?>
+									<?php $std_count->bindParam(1,$classroom_temp->class_id);?>
+									<?php $std_count->execute(); ?>
+									<span class="card-text">Student <?php echo $std_count->rowCount();?> คน</span>
 									<div class="mt-3 text-right text-size-28">										
 										<a href="ClassRoom.php?class_id=<?=$classroom_temp->class_id;?>" class="link_main1 mr-2"><i class="icon ion-ios-folder"></i></a>
-										<a href="#" class="link_main1"><i class="icon ion-trash-b"></i></a>
+										<a href="delete_classroom/server_delete_classroom.php?class_id=<?=$classroom_temp->class_id;?>" class="link_main1"><i class="icon ion-trash-b"></i></a>
 									</div>
 								</div>
 							</div>
