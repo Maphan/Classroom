b7ca27b850e497d121790a6bb477eb4f369bff02
jarLod <jaruwatpiew@hotmail.com>
2019-04-03 00:36:35 +0700

    update test.php

--- test.php
+++ test.php
@@ -1,5 +1,20 @@
 <?php
-	if("3">"2"){
-		echo "333333";
+include("connection/Connection.php");
+if(add_class_member("396dbe7db7","5930204324")){
+	echo "success";
+}else{
+	echo "fail";
+}
+
+function add_class_member($class_id, $std_id){
+	$stmt_insert_class_member=$GLOBALS['sql']->prepare("INSERT INTO class_member (class_id, std_id) VALUES (?,?)");
+	$stmt_insert_class_member->bindParam(1, $class_id);
+	$stmt_insert_class_member->bindParam(2, $std_id);
+	
+	if($stmt_insert_class_member->execute()){
+		return(true);
+	}else{
+		return(false);
 	}
+}
 ?>
