7b21800a348db3b829a0c71ca31f693a3d9308c0
jarLod <jaruwatpiew@hotmail.com>
2019-04-19 01:37:40 +0700

    update delete_class_owner

---
+++ teacher/delete_class_owner/server_delete_class_owner.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include("../../connection/Connection.php");
+include("../../classroom/func_delete_classroom.php");
+
+if(isset($_GET['class_id']) && isset($_GET['t_id']) ){
+	if(isset($_SESSION['permission']) && $_SESSION['permission']=="3"){
+		$class_id = $_GET['class_id'];
+		$t_id=$_GET['t_id'];
+		
+		if(delete_class_owner($class_id,$t_id)){
+        	header("Location: ../success.php");
+        }else{
+			echo "1";
+		}
+	}else{
+		echo "2";
+	}
+}else{
+	echo "3";
+}
+?>
